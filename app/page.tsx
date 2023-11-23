e879c3e861fcf5993350047d3e79fb8ec395b32b
tylers <tylersackel2505@gmail.com>
2023-11-22 21:50:50 -0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,10 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main>
-       <h1 className='p-4 m-4 bg-sky-400 text-white'>hello</h1>
+      <nav className=''>
+
+      </nav>
+       <h1 className='bg-sky-400 text-white'>hello</h1>
        <Link href={'/users'}>Users</Link>
     </main>
   )
