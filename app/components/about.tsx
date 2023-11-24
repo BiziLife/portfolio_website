b8732e9aeafff85952b374c7c8d5fb81ddb6a737
tylers <tylersackel2505@gmail.com>
2023-11-24 14:25:27 -0500

    first commit

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -1,21 +1,23 @@
-'use client';
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
-const about = () => {
+const About: React.FC = () => {
   return (
     <div>
-        <div className="hero min-h-screen bg-gray-700">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src="/Me.png" className="max-w-sm rounded-lg shadow-2xl bg-gray-200" />
-                <div>
-                <h1 className="text-5xl font-bold text-white">Box Office News!</h1>
-                <p className="py-6 text-white">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                <button className="btn bg-blue-600 text-white">Get Started</button>
-                </div>
-            </div>
+      <div className="hero min-h-screen bg-gray-700">
+        <div className="hero-content flex lg:flex-row items-center justify-between">
+          <div className="text-white max-w-md">
+            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <button className="btn bg-blue-600 text-white">Get Started</button>
+          </div>
+          <div>
+            <Image src="/Me.png" alt="img" width={400} height={400} />
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default about
+export default About;
