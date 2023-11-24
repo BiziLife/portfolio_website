e87fa140a0e17c55b7e6a530b0bfff05a01c8a68
tylers <tylersackel2505@gmail.com>
2023-11-24 15:06:38 -0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
 import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
 import Home from './components/Home'
 import About from './components/about'
 import Projects from './components/projects'
 import Footer from './components/footer'
-
-
-
-
 
 const page: React.FC = () => {
   return (
