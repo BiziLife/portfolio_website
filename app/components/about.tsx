bdc4b62f81a5028bcfc34a8a51f0b93114ca2391
tylers <tylersackel2505@gmail.com>
2023-11-24 16:16:57 -0500

    first commit

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -7,8 +7,8 @@ const About: React.FC = () => {
       <div className="hero min-h-screen bg-gray-700">
         <div className="hero-content flex lg:flex-row items-center justify-between">
           <div className="text-white max-w-md">
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <h1 className="text-5xl font-bold">Box Office News</h1>
+            <p className="py-6">Provident cupiditate voluptatem et in Quaerat fugiat ut assumenda excepturi exercitationem quasi In deleniti eaque aut repudiandae et a id nisi</p>
             <button className="btn bg-blue-600 text-white">Get Started</button>
           </div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import Home from './components/Home'
 import About from './components/about'
 import Projects from './components/projects'
-import Footer from './components/footer'
 
 const page: React.FC = () => {
   return (
     <div>
       <main>
        <Home />
-      
+       <About />
+       <Projects />
       </main>
     </div>
   );
