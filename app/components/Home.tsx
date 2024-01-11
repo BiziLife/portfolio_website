15c1fd1b2f929c724c7b5b7809463f2526454993
BiziLife <tylersackel2502@gmail.com>
2024-01-11 01:38:03 -0500

    Test

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -13,10 +13,10 @@ const Home: React.FC = () => {
             <h2 className="lg:mb-36 lg:text-2xl md:text-white md:text-base sm:text-base sm:mb-32 text-white">I love creating video games</h2>
             <button className="btn bg-blue-600 text-white lg:text-lg lg:w-48 md:text-base md:h-6 md:w-36 hover:bg-slate-800 transform hover:scale-110  transition-transform duration-300" >About me</button>
           </div>
-          <div className="hidden bg-slate-800 lg:flex justify-center max-w-sm rounded-lg shadow-xl ml-32 lg:ml-32 lg:mr-8">
+          <div className="hidden lg:flex justify-center max-w-sm rounded-lg shadow-xl ml-32 lg:ml-32 lg:mr-8">
              <Image src="/Me.png" alt="img" width={300} height={300} />
           </div>
-          <div className="lg:hidden bg-slate-800 flex justify-center max-w-sm rounded-lg shadow-xl ml-24">
+          <div className="lg:hidden flex justify-center max-w-sm rounded-lg shadow-xl ml-24">
             <Image src="/Me.png" alt="img" width={230} height={230} />
           </div>
 
