2bf85b09754eb39c5c76211f104507029d478c70
Tyler Sackel <78436840+BiziLife@users.noreply.github.com>
2024-05-13 19:09:08 -0400

    updated pages

--- src/app/ProjectsPage/page.tsx
+++ src/app/ProjectsPage/page.tsx
@@ -3,11 +3,11 @@ import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="mx-auto max-w-screen-lg">
       {/*I want the navigation bar here*/}
       < Navbar />
       {/*This will be the homepage of our website */}
-      <main className="">
+      <main className=" px-5 bg-white">
         <h1> Projects </h1>
       </main>
     </div>
