26aa6c4128540dccf723f744236ac477d11df346
Tyler Sackel <78436840+BiziLife@users.noreply.github.com>
2024-05-13 16:46:26 -0400

    navbar update

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,10 +3,10 @@ export default function Navbar(): JSX.Element {
     
     return (
         <>
-            <div id="navbar" className="sticky top-0 z-10 flex items-center justify-between px-5 py-5">
-                <a href="/" className="text-5xl">Tyler Sackel</a>
+            <div id="navbar" className="sticky top-0 z-10 flex items-center bg-white justify-between px-5 py-5 border-b-2 border-gray-300">
+                <a href="/" className="text-2xl loos-condensed-font">Tyler Sackel</a>
 
-                <ul className="flex space-x-4">
+                <ul className="flex space-x-4 text-base">
                     <li><a href="/BlogPage">Blog</a></li>
                     <li><a href="/ProjectsPage">Projects</a></li>
                     <li><a href="/ResumePage">Resume</a></li>
