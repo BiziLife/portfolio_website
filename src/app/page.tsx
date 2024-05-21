9248526dacad1b0b8f593761bf3a8646f556c138
Tyler Sackel <78436840+BiziLife@users.noreply.github.com>
2024-05-20 20:17:49 -0400

    updated main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,18 @@ export default function Home() {
       {/*I want the navigation bar here*/}
       < Navbar />
       {/*This will be the homepage of our website */}
-      <main className="px-5 bg-white">
+      <main className=" bg-white">
         <div>
-         <p className="text-4xl mt-10 font-bold"> Hi, I &apos;m Tyler </p>
+         <p className="text-6xl mt-10 font-bold"> Hi, I&apos;m Tyler </p>
          {/*Add image here*/}
-         <p></p>
+         <p className="text-sm roboto-bold text-gray-400 mt-3">Software engineering student at Liberty University</p>
+        </div>
 
-         
+        <div className="mt-20 text-lg">
+          <p className="mt-5"> I am a software engineering student at Liberty University with a passion for creating innovative projects using code, such as video games and websites. </p>
+          <p className="mt-5"> Since my teenage years, I&apos;ve been curious about how games are made, which sparked my journey into the world of programming. </p>
+          <p className="mt-5"> This curiosity has driven me to pursue an education at an amazing college, where I continue to deepen my knowledge and skills in software development. </p> 
+          <p className="mt-5"> In addition to my studies, I work as a team lead for the engineering computer services </p>
         </div>
         <br />
         <br />
